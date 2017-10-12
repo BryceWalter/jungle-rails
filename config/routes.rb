c4e71684059ada4ac32c2ad891d1c3be4391cdda
Bryce <brycewalterparker@gmail.com>
2017-10-12 22:52:01 +0000

    added screenshots, updated readme, removed admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,6 @@ Rails.application.routes.draw do
   resources :orders, only: [:create, :show]
 
   namespace :admin do
-    root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update]
   end
