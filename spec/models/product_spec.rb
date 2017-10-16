4cb10d2107637b5e8ccda3e92426212ee4726ac2
Bryce <brycewalterparker@gmail.com>
2017-10-16 16:05:06 +0000

    fixed bug in spec tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,28 +13,28 @@ RSpec.describe Product, type: :model do
       @category = Category.new(name: 'Junk')
       @product = Product.new(name: nil, price: 34.50, quantity: 25, category: @category)
       @product.save
-      expect(@product.errors.full_messages.include?("Name can't be blank")).to be_present
+      expect(@product.errors.full_messages).to include "Name can't be blank"
     end
 
     it 'Should throw error if price is not present' do
       @category = Category.new(name: 'Junk')
       @product = Product.new(name: 'Clock', price: nil, quantity: 25, category: @category)
       @product.save
-      expect(@product.errors.full_messages.include?("Price can't be blank")).to be_present
+      expect(@product.errors.full_messages).to include "Price can't be blank"
     end
 
     it 'Should throw error if quantity is not present' do
       @category = Category.new(name: 'Junk')
       @product = Product.new(name: 'Clock', price: 34.50, quantity: nil, category: @category)
       @product.save
-      expect(@product.errors.full_messages.include?("Quantity can't be blank")).to be_present
+      expect(@product.errors.full_messages).to include "Quantity can't be blank"
     end
 
     it 'Should throw error if category is not present' do
       @category = Category.new(name: 'Junk')
       @product = Product.new(name: 'Clock', price: 34.50, quantity: 25, category: nil)
       @product.save
-      expect(@product.errors.full_messages.include?("Category can't be blank")).to be_present
+      expect(@product.errors.full_messages).to include "Category can't be blank"
     end
   end
 end
