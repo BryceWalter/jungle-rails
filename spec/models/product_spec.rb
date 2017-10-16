76681005187cde4e625eb7cd8f0dece653ba8fff
Bryce <brycewalterparker@gmail.com>
2017-10-16 15:52:40 +0000

    added product model spec testing for presence of errors

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'Should save a new product' do
+      @category = Category.new(name: 'Junk')
+      @product = Product.new(name: 'Clock', price: 34.50, quantity: 25, category: @category)
+      @product.save!
+      expect(@product.id).to be_present
+    end
+
+    it 'Should throw error if name is not present' do
+      @category = Category.new(name: 'Junk')
+      @product = Product.new(name: nil, price: 34.50, quantity: 25, category: @category)
+      @product.save
+      expect(@product.errors.full_messages.include?("Name can't be blank")).to be_present
+    end
+
+    it 'Should throw error if price is not present' do
+      @category = Category.new(name: 'Junk')
+      @product = Product.new(name: 'Clock', price: nil, quantity: 25, category: @category)
+      @product.save
+      expect(@product.errors.full_messages.include?("Price can't be blank")).to be_present
+    end
+
+    it 'Should throw error if quantity is not present' do
+      @category = Category.new(name: 'Junk')
+      @product = Product.new(name: 'Clock', price: 34.50, quantity: nil, category: @category)
+      @product.save
+      expect(@product.errors.full_messages.include?("Quantity can't be blank")).to be_present
+    end
+
+    it 'Should throw error if category is not present' do
+      @category = Category.new(name: 'Junk')
+      @product = Product.new(name: 'Clock', price: 34.50, quantity: 25, category: nil)
+      @product.save
+      expect(@product.errors.full_messages.include?("Category can't be blank")).to be_present
+    end
+  end
+end
